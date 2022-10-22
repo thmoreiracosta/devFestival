43b20bf147f6d797494937034e2a0c7c210a5fdb
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-10-22 19:03:10 -0300

    Main css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,16 @@ import tema from '../assets/darkmode.png';
 
 export function Header() {
   return (
-    <header className='flex bg-black text-center items-center justify-center space-x-96 text-white'>      
+    <header className='flex gap-4 bg-black text-center items-center justify-center space-x-96 text-white'>      
         <div>
           <img src={logo} alt="logo" />
         </div>
-        <div>
-          <span>sobre</span>
-          <input type="text" />
+        <div className='flex gap-8 justify-between text-center items-center'>
+          <span className='uppercase gap-4'>sobre</span>
+          <button
+          className='bg-gray-800 rounded-[255px] w-[254px] h-[45px] gap-10'>          
+            Quero meu ingresso!
+            </button>
           <button>
             <img src={tema} alt="" />
           </button>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,30 @@
+import festival_dev from '../assets/festival_dev.png';
+import pato from '../assets/pato.png';
 
 export function Main() {
   return (
-    <div>
-      <h1 className="text-4xl text-blue-100 font-sans font-bold "
-      >
-        &lt;Dev/&gt;
-      </h1>
-      <h2 className='text-4xl text-black font-bold'>
-        Festival
-      </h2>
-    </div>
+    <main className='flex gap-40 max-h-8 w-full mt-20 justify-center'>
+      <div className=''>
+        <img src={pato} alt="" />
+        <h1 className="text-4xl text-blue-100 font-bold "
+        >
+          &lt;Dev/&gt;
+        </h1>
+        <h2 className='text-4xl text-black font-bold'>
+          Festival
+        </h2>
+        <button className="bg-blue-100 text-white rounded-[255px] w-[274px] h-[61px]">
+          Quero meu ingresso!
+        </button>
+      </div>
+
+      <div>
+        <img
+          className='w-[415px] h-[590px]'
+          src={festival_dev}
+          alt=""           
+          />
+      </div>
+    </main>
   )
 }
