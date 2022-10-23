092fc5dc5e13080c492ba8a898e8b98f2e30581d
Thiago Costa <thiagomoreiracosta@gmail.com>
2022-10-22 22:18:48 -0300

    Aboult complet

--- src/components/Aboult.tsx
+++ src/components/Aboult.tsx
@@ -3,7 +3,7 @@ import aboult from '../assets/aboult_dev.png';
 
 export function Aboult() {
   return (
-    <div className='flex mt-20 border gap-20 justify-center items-center'>
+    <div className='flex mt-20 mb-20 gap-20 justify-center items-center '>
       <div>
         <img
           className='w-[415px] h-[590px]'
@@ -12,16 +12,25 @@ export function Aboult() {
         />
       </div>
 
-      <div className='w-[400px] justify-center border '>
+      <div id="aboult" className='w-[400px] justify-center border '>
         <h1 className="text-5xl text-black font-bold p-2 border w-[400px]"
         >
-          Sobre <br/>o festival
+          Sobre <br />o festival
         </h1>
-        <p className='border w-[400px]'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Eius deleniti mollitia eligendi odio commodi vel! Amet, dicta.
-          Impedit, veritatis ipsa ratione sint qui inventore consectetur non, reprehenderit ex molestiae consequuntur!
-        </p>        
+        <div className='text-justify text-base mt-4'>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Eius deleniti mollitia eligendi odio commodi vel! Amet, dicta.
+            Impedit, veritatis ipsa ratione sint qui inventore consectetur non, reprehenderit ex molestiae consequuntur!
+          </p>
+          <br />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Aperiam doloremque eos sit praesentium necessitatibus nobis quam molestiae in iusto voluptatem, velit reiciendis at quia?
+            Cum eum ipsam dolorum aut nam.
+          </p>
+        </div>
       </div>
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,26 @@ import tema from '../assets/darkmode.png';
 
 export function Header() {
   return (
-    <header className='flex gap-4 bg-black text-center items-center justify-center space-x-96 text-white'>      
-        <div className='mt-2 w-[150px] h-[70px] justify-center items-center text-center'>
+    <header className='flex gap-4 bg-black text-center items-center justify-center space-x-96 text-white h-24'>
+      <div className='mt-2 w-[150px] h-[70px] justify-center items-center text-center cursor-pointer'>
+        <a href="https://www.kenzie.com.br" target='_black'>
+
           <img src={logo} alt="logo" />
-        </div>
-        <div className='flex gap-8 justify-between text-center items-center'>
-          <span className='uppercase gap-4'>sobre</span>
-          <button
-          className='text-violet-100 border-solid border-2 border-violet-100 rounded-[255px] w-[254px] h-[45px] gap-10'>          
-            Quero meu ingresso!
-            </button>
-          <button>
-            <img src={tema} alt="" />
-          </button>
-        </div>      
+        </a>
+      </div>
+      <div className='flex gap-8 justify-between text-center items-center'>
+        <a href="#aboult">
+
+          <span className='uppercase gap-4 cursor-pointer'>sobre</span>
+        </a>
+        <button
+          className='text-violet-100 hover:bg-yellow-400 hover:text-slate-800 border-solid border-2 border-violet-100 rounded-[255px] w-[254px] h-[45px] gap-10'>
+          Quero meu ingresso!
+        </button>
+        <button>
+          <img src={tema} alt="" />
+        </button>
+      </div>
     </header>
   )
 }
